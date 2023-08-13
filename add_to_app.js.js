17d65012f3594e8b14b193c135523f13d377cfca
Nicholas <aigberua@gmail.com>
2023-08-14 00:19:47 +0100

    Fixes

--- add_to_app.js.js
+++ add_to_app.js.js
@@ -1,22 +1,22 @@
-// const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer');
 
-// (async () => {
-//     const browser = await puppeteer.launch();
-//     const page = await browser.newPage();
+(async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
     
-//     // Increase the navigation timeout to 60 seconds
-//     await page.setDefaultNavigationTimeout(60000); // 60 seconds
+    // Increase the navigation timeout to 60 seconds
+    await page.setDefaultNavigationTimeout(60000); // 60 seconds
     
-//     try {
-//         await page.goto('link');
+    try {
+        await page.goto('https://github.com/Ghosteken');
 
-//         // Wait for a specific element to ensure the page is fully loaded
-//       // await page.waitForSelector('YOUR_SELECTOR_HERE', { timeout: 60000 }); // Replace with an actual selector
+        // Wait for a specific element to ensure the page is fully loaded
+        await page.waitForSelector('YOUR_SELECTOR_HERE', { timeout: 60000 }); // Replace with an actual selector
 
-//         console.log('Page loaded successfully.');
-//     } catch (error) {
-//         console.error('Error navigating to the page:', error);
-//     }
+        console.log('Page loaded successfully.');
+    } catch (error) {
+        console.error('Error navigating to the page:', error);
+    }
     
-//     await browser.close();
-// })();
+    await browser.close();
+})();
